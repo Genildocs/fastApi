d9002bab190b2236f9d7509cb0029f1f55d72211
genildocs <genildocs@gmail.com>
2024-08-18 17:20:45 -0300

    commit

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+
+class Product(BaseModel):
+    name: str
+    price: float
+    description: str | None = None
 
 app = FastAPI()
 
@@ -13,7 +19,7 @@ async def root(request: Request):
     return templates.TemplateResponse("index.html",{
         "request": request,
         "title": "Fruitables",
-        "message": "Fake api de produtos de hortfruit"
+        "message": "Fake api of fruit and vegetable products"
     })
 
 @app.get("/api/v1/products")
@@ -26,3 +32,12 @@ async def read_item(item_id: int):
         return {"item_id": item_id}
     else:
         return {"message": "Item not found"}
+
+
+@app.post("/api/v1/products")
+async def create_product(product: Product):
+    return product
+
+@app.delete("/api/v1/products/{item_id}")
+async def delete_item(item_id: int):
+    return {"message: No items to delete"}
