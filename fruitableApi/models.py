df79a392784a86436e69459831abe1773952e129
genildocs <genildocs@gmail.com>
2024-08-20 07:53:02 -0300

    commit

--- fruitableApi/models.py
+++ fruitableApi/models.py
@@ -9,10 +9,15 @@ class Product(Base):
     name = Column(String, index=True)
     price = Column(Integer)
     description = Column(String)
+    category = Column(String)
+    quantity = Column(Integer, default=0)
+    disponible = Column(Boolean , default=True)
+   
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, unique=True)
+    username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
